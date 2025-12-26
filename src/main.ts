e2c104ac180a49cd48810bc8956b23d2fb030477
JeanCarlosBezerra <cjeanbezerra@gmail.com>
2025-12-26 15:15:09 -0300

    commit ajustes backend

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@
     app.setGlobalPrefix('api');
 
     app.enableCors({
-      origin: 'http://localhost:3000',
+      origin: ['http://localhost:3000', 'http://172.28.7.6:3000'],
       credentials: true,
       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
       allowedHeaders: ['Content-Type', 'Authorization'],
